7b0f739db72275bb04c896a127d94fa4b448382b
SeanBrown <srbrown@umass.edu>
2025-02-28 16:22:52 -0500

    edited table

--- app.py
+++ app.py
@@ -330,7 +330,7 @@ def add_tag_location():
             c.execute("""
                 INSERT INTO tags (tag_name, user_id, latitude, longitude)
                 VALUES (?, ?, ?, ?)
-            """, (tag_name, user_id, 0.0, 0.0))  # Default location set to 0.0, 0.0
+            """, (tag_name, user_id, x_offset, y_offset))  # Default location set to 0.0, 0.0
 
         # Fetch the anchor's GPS coordinates
         #c.execute("SELECT latitude, longitude FROM anchors WHERE id=?", (anchor_id,))
