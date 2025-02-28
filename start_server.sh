4f64b768651ce4e7a2ec68557b83796355a14bf5
Ken Su <kensu324@gmail.com>
2025-02-28 10:47:51 -0500

    updated server to use local ip address

--- start_server.sh
+++ start_server.sh
@@ -6,22 +6,23 @@ source ~/Documents/BareTag_BackEnd/venv/bin/activate
 sleep 1
 
 # Turn on the flask server (e.g. test_app.py)
-nohup python app.py > flask.log 2>&1 &
+# nohup python app.py > flask.log 2>&1 &
+nohup python app.py --host=172.24.131.25 --port=5000 > flask.log 2>&1 &
 
 sleep 1
 
 # Kills any running ngrok processes
-pkill -f ngrok  
+# pkill -f ngrok  
 
 # Turn on the ngrok server
-ngrok http --domain=vital-dear-rattler.ngrok-free.app 5000 > ngrok.log 2>&1 &
+# ngrok http --domain=vital-dear-rattler.ngrok-free.app 5000 > ngrok.log 2>&1 &
 # ngrok http 5000 > ngrok.log 2>&1 &
 
 # Wait for ngrok to initialize
 sleep 2
 
 # Get the ngrok URL automatically
-NGROK_URL=$(curl -s http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+# NGROK_URL=$(curl -s http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
 
 # Print the correct ngrok URL
-echo "✅ Your ngrok URL is: $NGROK_URL"
+# echo "✅ Your ngrok URL is: $NGROK_URL"
