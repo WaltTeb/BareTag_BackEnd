7b7165a5c2c6f00bd82f17dd02b5abbbef1875d7
Ken Su <kensu324@gmail.com>
2025-03-04 12:37:55 -0500

    modified /get_tags to return string IDS

--- app.py
+++ app.py
@@ -546,7 +546,7 @@ def get_tag_location():
         result = []
         for tag in tags_locations:
             result.append({
-                'id': tag[0],
+                'id': str(tag[0]),
                 'tag_name': tag[1],
                 'latitude': tag[2],
                 'longitude': tag[3]
